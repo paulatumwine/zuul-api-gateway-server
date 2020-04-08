9f42a789c831aef76d9c2118c88970377218dfd3
Paul A. Tumwine <paulatumwine@gmail.com>
2020-04-08 12:49:35 -0500

    Fix logging in ZuulFilter

--- src/main/java/com/samples/microservices/zuulapigatewayserver/filter/ZuulLoggingFilter.java
+++ src/main/java/com/samples/microservices/zuulapigatewayserver/filter/ZuulLoggingFilter.java
@@ -5,9 +5,11 @@ import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Component
 public class ZuulLoggingFilter extends ZuulFilter {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
